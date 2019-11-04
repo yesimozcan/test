78176f5e3d496e56e4dd5c175c84fcdcd558911f
yesimozcan <38218352+yesimozcan@users.noreply.github.com>
2019-11-04 14:17:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,11 @@ with open("list") as url:
 			if response.status_code == 200 and re.search('couchdb',str(response.content)) != None:
 				allDbs=requests.get(i+"/_all_dbs").json()
 				for x in allDbs:
-					if len(allDbs) > 1:
-						response2=requests.get(i+"/"+x)
-						if response2.status_code == 200:
-							sonuc=(response2.url,response2.status_code)
-							f=open("cikti.txt","a")
-							f.write(str(sonuc)+"\n")
-							f.close()
+					response2=requests.get(i+"/"+x)
+					if (len(allDbs) > 1) and (response2.status_code == 200):
+						sonuc=(response2.url,response2.status_code)
+						f=open("ciktim.txt","a")
+						f.write(str(sonuc)+"\n")
+						f.close()
 		except IOError as e:
 			pass
