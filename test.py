27ea76a2b7b43fa2c5395e537cdeb18629866333
yesimozcan <38218352+yesimozcan@users.noreply.github.com>
2019-11-04 13:44:30 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 import requests
 import re
-with open("ll.txt") as url:
+import json
+with open("list") as url:
 	for i in url.read().split("\n"):
-		dblist=[]
-		response=requests.get(i)
-		if response.status_code == 200 and re.search('couchdb',str(response.content)) != None:
-			allDbs=requests.get(i+"/_all_dbs")
-			dblist.append(allDbs.content)
-print (dblist)
+		try:
+			response=requests.get(i)
+			if response.status_code == 200 and re.search('couchdb',str(response.content)) != None:
+				allDbs=requests.get(i+"/_all_dbs").json()
+				for x in allDbs:
+					if len(allDbs) > 1:
+						response2=requests.get(i+"/"+x)
+						if response2.status_code == 200:
+							sonuc=(response2.url,response2.status_code)
+							f=open("cikti.txt","a")
+							f.write(str(sonuc)+"\n")
+							f.close()
+		except IOError as e:
+			pass
